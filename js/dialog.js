f6e63646ca04be0d19b1fb79b148b6f53546bc87
mateusmaciel340 <mateusmaciel340@gmail.com>
2023-05-10 17:43:55 -0300

    feat: modal acessível

--- js/dialog.js
+++ js/dialog.js
@@ -1,19 +1,36 @@
 // Variáveis
+var conteudoForaDialog = document.querySelector('#conteudoForaDialog');
+var video = document.querySelector('video');
 var btnAbreDialog = document.querySelector('#abreDialog');
 var dialog = document.querySelector('.dialogNewsletter');
 var dialogBody = document.querySelector('.dialogNewsletter-body');
 var dialogOverlay = document.querySelector('.dialogNewsletter-overlay');
 
+btnAbreDialog.style.display = 'block';
+
 
 // Quando abrir a dialog...
 btnAbreDialog.addEventListener('click', function() {
   dialog.classList.add('dialogNewsletter--aberto');
+  document.querySelector('.dialogNewsletter-campo').focus();
+  conteudoForaDialog.inert = true;
+
+  video.removeAttribute('controls');
 });
 
 function fechandoDialog() {
   document.activeElement.blur();
   dialog.classList.remove('dialogNewsletter--aberto');     
+  conteudoForaDialog.inert = false;
+  video.setAttribute('controls', true);
 }
 
 // Listeners
 document.querySelector('.dialogNewsletter-fechar').addEventListener('click', fechandoDialog);
+dialogOverlay.addEventListener('click', fechandoDialog);
+
+document.addEventListener('keyup', function (evento) {
+  if (evento.code == 'Escape') {
+    fechandoDialog();
+  }
+});
